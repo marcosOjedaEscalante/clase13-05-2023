4c4a28940ef85b9023c01583dd6746e1991cc9c3
Marcos Ojeda Escalante <marcos.ojeda.escalante@gmail.com>
2023-05-17 21:41:04 -0400

    escritura archivos JSON con colaboración de clases

--- js/funcionalidades.js
+++ js/funcionalidades.js
@@ -2,16 +2,50 @@ import { readFileSync } from "fs";
 import { writeFileSync } from "fs";
 import { parse } from "csv-parse/sync";
 import { Curso } from "./model/curso.js";
+import { Relator } from "./model/relator.js";
+import { Ayudante } from "./model/ayudante.js";
+import { Estudiante } from "./model/estudiante.js";
 
 const leerArchivo = nombreArchivo => parse(readFileSync(`./data/${nombreArchivo}.csv`, 'utf-8'));
 
 const construirCurso = arregloDeArreglosCursos => arregloDeArreglosCursos.map(curso => curso = new Curso(curso[0],curso[1],curso[2]));
+
+const construirRelator = arregloDeArreglosRelatores => arregloDeArreglosRelatores.map(relator => relator = new Relator(relator[0],relator[1],relator[2],relator[3]));
+
+const construirAyudante = arregloDeArreglosAyudantes => arregloDeArreglosAyudantes.map(ayudante => ayudante = new Ayudante(ayudante[0],ayudante[1],ayudante[2],ayudante[3]));
+
+const construirEstudiante = arregloDeArreglosEstudiantes => arregloDeArreglosEstudiantes.map(estudiante => estudiante = new Estudiante(estudiante[0],estudiante[1],estudiante[2],estudiante[3]));
+
+const construirCursoColaboracion = (arregloCursos, arregloRelatores, arregloAyudantes, arregloEstudiantes) =>{
+    arregloCursos.forEach((curso) => {
+        arregloRelatores.forEach((relator) => {
+            if(curso.codigo === relator.codigoCurso){
+                curso.relator = relator;
+            }
+        });
+        arregloAyudantes.forEach((ayudante) => {
+            if(curso.codigo === ayudante.codigoCurso){
+                curso.ayudante = ayudante;
+            }
+        });
+        arregloEstudiantes.forEach((estudiante) => {
+            if(curso.codigo === estudiante.codigoCurso){
+                curso.estudiantes = estudiante;
+            }
+        });
+    });
+    return arregloCursos;
+}
 
 const escribirJSON = arregloCursos => writeFileSync('./dataJSON/cursos.json', JSON.stringify(arregloCursos));
 
 export {
     leerArchivo,
     construirCurso,
+    construirRelator,
+    construirAyudante,
+    construirEstudiante,
+    construirCursoColaboracion,
     escribirJSON
 }
 
--- js/index.js
+++ js/index.js
@@ -1,5 +1,11 @@
 import { leerArchivo } from "./funcionalidades.js";
-import { construirCurso } from "./funcionalidades.js";
+import { construirCurso, construirAyudante, construirRelator, construirEstudiante, construirCursoColaboracion } from "./funcionalidades.js";
 import { escribirJSON } from "./funcionalidades.js";
 
-escribirJSON(construirCurso(leerArchivo('cursos')));
+/* console.log(construirCurso(leerArchivo('cursos')));
+console.log(construirRelator(leerArchivo('relatores')));
+console.log(construirAyudante(leerArchivo('ayudantes')));
+console.log(construirEstudiante(leerArchivo('estudiantes'))); */
+/* escribirJSON(construirCurso(leerArchivo('cursos'))); */
+
+escribirJSON(construirCursoColaboracion(construirCurso(leerArchivo('cursos')), construirRelator(leerArchivo('relatores')), construirAyudante(leerArchivo('ayudantes')), construirEstudiante(leerArchivo('estudiantes'))));
